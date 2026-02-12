fecda26dc9cf7da1dbd8b969dd925e54e8ecca49
PepperSan <lookwithad@gmail.com>
2026-02-12 15:08:43 +0200

    feat: implement multithreaded simulation engine with scheduled ticks

--- src/main/java/island/simulation/Main.java
+++ src/main/java/island/simulation/Main.java
@@ -1,22 +1,15 @@
 package island.simulation;
 
 import island.model.island.Island;
+import island.simulation.engine.SimulationEngine;
 import island.simulation.init.IslandPopulator;
-import island.simulation.service.FeedingService;
 
 public class Main {
     public static void main(String[] args) {
         Island island = new Island(3, 3);
         new IslandPopulator().populate(island);
 
-        System.out.println("BEFORE:");
-        System.out.println("Animals in (1,1): " + island.getLocation(1, 1).getAnimals().size());
-        System.out.println("Plants in (1,1): " + island.getLocation(1, 1).getPlants().size());
-
-        new FeedingService().feed(island);
-
-        System.out.println("AFTER:");
-        System.out.println("Animals in (1,1): " + island.getLocation(1, 1).getAnimals().size());
-        System.out.println("Plants in (1,1): " + island.getLocation(1, 1).getPlants().size());
+        SimulationEngine engine = new SimulationEngine(island);
+        engine.start();
     }
 }
